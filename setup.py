b120bfc7a936b221a0e2bb0e29a453bb77416903
Cameron Currie <me@cameroncurrie.net>
2015-05-26 01:07:07 -0500

    Release 1.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name='coderwall',
-        version='1.2.0',
+        version='1.3.0',
         author='Cameron Currie',
         author_email='me@cameroncurrie.net',
         url='http://github.com/cwc/coderwall-python',
